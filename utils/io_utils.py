4eb478e1c19ef0fd09a2f1097d7744a000152a83
arjunbhagoji <arjunbhagoji@gmail.com>
2019-10-30 18:31:51 -0400

    adding dropping io

--- utils/io_utils.py
+++ utils/io_utils.py
@@ -11,7 +11,9 @@ def init_dirs(args):
 	if args.rand_init:
 		model_name += 'rand'
 	if args.n_classes != 10:
-		model_name += '_cl' + str(args.n_classes) 
+		model_name += '_cl' + str(args.n_classes)
+	if args.dropping:
+		model_name += '_matching' 
 	model_dir_name = args.checkpoint_path + '_' + args.dataset_in + '/' + model_name
 	log_dir_name = 'logs_' + args.dataset_in + '/' +model_name
 	if not os.path.exists(model_dir_name):
@@ -22,6 +24,7 @@ def init_dirs(args):
 	log_dir_name += '/'
 	return model_dir_name, log_dir_name
 		
-def matching_file_name(args):
-    matching_file_name = args.matching_path + '/' + str(class_1) + '_' + str(class_2) + '_' + str(args.num_samples) + '_' + args.dataset_in + '_' + args.norm + '_' + '{0:.1f}.npy'.format(eps)
-	'matchings/' + save_file_name + 
+def matching_file_name(args, class_1, class_2):
+    matching_file_name = args.matching_path + '/' + str(class_1) + '_' + str(class_2) + '_' + str(args.num_samples) + '_' + args.dataset_in + '_' + args.norm + '_cost_zero_' + '{0:.1f}.npy'.format(args.epsilon)
+
+    return matching_file_name
