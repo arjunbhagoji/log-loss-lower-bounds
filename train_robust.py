d19cb29980aa65bd25443e270dbfb053f1b5c31f
arjunbhagoji <arjunbhagoji@gmail.com>
2019-12-30 19:17:14 -0500

    added args for tracking hard points

--- train_robust.py
+++ train_robust.py
@@ -28,8 +28,8 @@ if __name__ == '__main__':
     
     # Data args
     parser.add_argument('--dataset_in', type=str, default='MNIST')
-    parser.add_argument('--n_classes', type=int, default=10)
-    parser.add_argument('--num_samples', type=int, default=None)
+    parser.add_argument('--n_classes', type=int, default=2)
+    parser.add_argument('--num_samples', type=int, default=4000)
 
     # Model args
     parser.add_argument('--model', type=str, default='cnn_3l', choices=['wrn','cnn_3l', 'dn'])
@@ -37,6 +37,7 @@ if __name__ == '__main__':
     parser.add_argument('--fc_expand', type=int, default=1)
     parser.add_argument('--depth', type=int, default=28)
     parser.add_argument('--width', type=int, default=10)
+    parser.add_argument('--trial_num', type=int, default=None)
 
     # Training args
     parser.add_argument('--batch_size', type=int, default=128) 
@@ -48,10 +49,10 @@ if __name__ == '__main__':
 
     # Attack args
     parser.add_argument('--is_adv', dest='is_adv', action='store_true')
-    parser.add_argument('--attack', type=str, default='PGD_linf')
-    parser.add_argument('--epsilon', type=float, default=8.0)
+    parser.add_argument('--attack', type=str, default='PGD_l2')
+    parser.add_argument('--epsilon', type=float, default=3.0)
     parser.add_argument('--attack_iter', type=int, default=10)
-    parser.add_argument('--eps_step', type=float, default=2.0)
+    parser.add_argument('--eps_step', type=float, default=0.3)
     parser.add_argument('--targeted', dest='targeted', action='store_true')
     parser.add_argument('--clip_min', type=float, default=0)
     parser.add_argument('--clip_max', type=float, default=1.0)
@@ -61,20 +62,25 @@ if __name__ == '__main__':
     # IO args
     parser.add_argument('--last_epoch', type=int, default=0)
     parser.add_argument('--curr_epoch', type=int, default=0)
+    parser.add_argument('--save_checkpoint', dest='save_checkpoint', action='store_true')
     parser.add_argument('--load_checkpoint', dest='load_checkpoint', action='store_true')
     parser.add_argument('--checkpoint_path', type=str, default='trained_models')
 
     # Matching args
+    parser.add_argument('--track_hard', dest='track_hard', action='store_true')
     parser.add_argument('--is_dropping', dest='dropping', action='store_true')
     parser.add_argument('--dropping_strat', type=str, default='matched')
     parser.add_argument('--matching_path', type=str, default='matchings')
-    parser.add_argument('--degree_path', type=str, default='degree_results')
+    parser.add_argument('--degree_path', type=str, default='graph_data/degree_results')
     parser.add_argument("--norm", default='l2', help="norm to be used")
     parser.add_argument('--drop_thresh', type=int, default=100)
     
     args = parser.parse_args()
+    if args.num_samples is None:
+        args.num_samples = 'All'
     model_dir_name, log_dir_name, _ = init_dirs(args)
-    writer = SummaryWriter(log_dir=log_dir_name)
+    if args.save_checkpoint:
+        writer = SummaryWriter(log_dir=log_dir_name)
     print('Training %s' % model_dir_name)
 
     if torch.cuda.is_available():
@@ -133,7 +139,7 @@ if __name__ == '__main__':
         net.load_state_dict(torch.load(model_dir_name + ckpt_path))
         robust_test_during_train(net, loader_test, args, n_batches=10)
 
-    criterion = nn.CrossEntropyLoss()  
+    criterion = nn.CrossEntropyLoss(reduction='none')  
 
     optimizer = torch.optim.SGD(net.parameters(),
                                 lr=args.learning_rate,
@@ -148,21 +154,24 @@ if __name__ == '__main__':
         if not args.is_adv:
             curr_loss = train_one_epoch(net, criterion, optimizer, 
                                   loader_train, verbose=False)
+            ben_loss = curr_loss
         else:
             curr_loss, ben_loss = robust_train_one_epoch(net, criterion, optimizer, loader_train, args, eps, delta, verbose=False)
         print('time_taken for #{} epoch = {:.3f}'.format(epoch+1, time.time()-start_time))
         acc, acc_adv, test_loss, test_loss_adv = robust_test_during_train(net, criterion, loader_test, args, n_batches=10)
         ckpt_path = 'checkpoint_' + str(args.last_epoch)
-        torch.save(net.state_dict(), model_dir_name + ckpt_path)
-        writer.add_scalar('Loss/train', curr_loss, epoch)
-        writer.add_scalar('Loss/test', test_loss, epoch)
-        writer.add_scalar('Loss/test_adv', test_loss_adv, epoch)
-        writer.add_scalar('Acc/test', acc, epoch)
-        writer.add_scalar('Acc/test_adv', acc_adv, epoch)
-        writer.add_scalar('Lr', lr, epoch)
-        if args.is_adv:
-            writer.add_scalar('Loss/train_ben', ben_loss, epoch)
-        else:
-            writer.add_scalar('Loss/train_ben', 0, epoch)
-        print(curr_loss, test_loss, test_loss_adv, acc, acc_adv, lr, ben_loss)
+        if args.save_checkpoint:
+            torch.save(net.state_dict(), model_dir_name + ckpt_path)
+            writer.add_scalar('Loss/train', curr_loss, epoch)
+            writer.add_scalar('Loss/test', test_loss, epoch)
+            writer.add_scalar('Loss/test_adv', test_loss_adv, epoch)
+            writer.add_scalar('Acc/test', acc, epoch)
+            writer.add_scalar('Acc/test_adv', acc_adv, epoch)
+            writer.add_scalar('Lr', lr, epoch)
+            if args.is_adv:
+                writer.add_scalar('Loss/train_ben', ben_loss, epoch)
+            else:
+                writer.add_scalar('Loss/train_ben', 0, epoch)
+        print('Train loss - Adv: %s Ben: %s; Test loss - Adv: %s; Ben: %s' %
+            (curr_loss, ben_loss, test_loss_adv, test_loss))
 
