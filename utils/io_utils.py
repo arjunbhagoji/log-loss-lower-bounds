b4f902a2272e1478e8b1f2f2694636e96418dbc2
arjunbhagoji <arjunbhagoji@gmail.com>
2019-12-30 19:20:57 -0500

    approx added to save name

--- utils/io_utils.py
+++ utils/io_utils.py
@@ -13,29 +13,29 @@ def init_dirs(args):
 		model_name += '_robust' + '_eps' + str(args.epsilon) + '_k' + str(args.attack_iter) + '_delta' + str(args.eps_step)
 	if args.eps_schedule != 0:
 		model_name += '_eps_sched' + str(args.eps_schedule)
-	# if args.is_adv:
-	# 	model_name += '_robust' + '_eps' + str(args.epsilon)
 	if args.rand_init:
 		model_name += '_rand'
 	if args.n_classes != 10:
 		model_name += '_cl' + str(args.n_classes)
+	if args.num_samples != 2000:
+		model_name += '_ns' + str(args.num_samples)
 	if args.dropping:
 		if args.dropping_strat == 'matched':
 			model_name += '_matching'
 		elif args.dropping_strat == 'approx':
-			model_name += '_approx' + str(args.drop_thresh) 
-	model_dir_name = args.checkpoint_path + '_' + args.dataset_in + '/' + model_name
-	log_dir_name = 'logs_' + args.dataset_in + '/' + model_name
-	figure_dir_name = 'images_' + args.dataset_in + '/' + model_name
-	if not os.path.exists(model_dir_name):
-		os.makedirs(model_dir_name)
-	if not os.path.exists(log_dir_name):
-		os.makedirs(log_dir_name)
-	if not os.path.exists(figure_dir_name):
-		os.makedirs(figure_dir_name)
+			model_name += '_approx' + str(args.drop_thresh)
+	if args.trial_num is not None:
+		model_name += '_tr' + str(args.trial_num) 
+	model_dir_name = args.checkpoint_path + '/' + args.dataset_in + '/' + model_name
+	log_dir_name = 'logs' + '/' + args.dataset_in + '/' + model_name
+	figure_dir_name = 'images' + '/' + args.dataset_in + '/' + model_name
+	if args.save_checkpoint:
+		if not os.path.exists(model_dir_name):
+			os.makedirs(model_dir_name)
+		if not os.path.exists(log_dir_name):
+			os.makedirs(log_dir_name)
 	model_dir_name += '/'
 	log_dir_name += '/'
-	figure_dir_name += '/'
 	return model_dir_name, log_dir_name, figure_dir_name
 		
 def matching_file_name(args, class_1, class_2):
