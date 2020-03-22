20e51e50c9b18b7e17a5cfb8e78dc7f4b97a5ad8
arjunbhagoji <arjunbhagoji@gmail.com>
2020-03-21 20:55:22 -0400

    hybrid attack

--- utils/attack_utils.py
+++ utils/attack_utils.py
@@ -90,7 +90,8 @@ def pgd_attack(model, image_tensor, img_variable, tar_label_variable,
     return best_adv_x
 
 def pgd_l2_attack(model, image_tensor, img_variable, tar_label_variable,
-               n_steps, eps_max, eps_step, clip_min, clip_max, targeted, rand_init):
+               n_steps, eps_max, eps_step, clip_min, clip_max, targeted, 
+               rand_init, num_restarts, image_tensor_mod=None, unmod_indices=None):
     """
     image_tensor: tensor which holds the clean images. 
     img_variable: Corresponding pytorch variable for image_tensor.
@@ -99,39 +100,77 @@ def pgd_l2_attack(model, image_tensor, img_variable, tar_label_variable,
     eps_max: maximum l_inf attack perturbations. 
     eps_step: l_inf attack perturbation per step
     """
+    
     best_losses = None
     best_adv_x = None
+    image_tensor_orig = image_tensor.clone().detach()
+    tar_label_orig = tar_label_variable.clone().detach()
 
-    if rand_init:
-        img_variable = rand_init_l2(img_variable, eps_max)
+    for j in range(num_restarts):
+        if rand_init:
+            img_variable = rand_init_l2(img_variable, eps_max)
 
-    output = model.forward(img_variable)
-    for i in range(n_steps):
-        zero_gradients(img_variable)
+        if image_tensor_mod is not None:
+            # print('Using hybrid attack')
+            image_tensor_init = torch.cat((img_variable.data[unmod_indices],
+                                            image_tensor_mod))
+            image_tensor = torch.cat((image_tensor_orig[unmod_indices], 
+                                        image_tensor_orig[~unmod_indices])) 
+            tar_label_variable = torch.cat((tar_label_orig[unmod_indices],
+                                        tar_label_orig[~unmod_indices]))
+            img_variable = Variable(image_tensor_init, requires_grad=True)
+        else:
+            image_tensor = image_tensor_orig
+            
         output = model.forward(img_variable)
-        loss_cal, losses_cal = cal_loss(output, tar_label_variable, targeted)
+        for i in range(n_steps):
+            zero_gradients(img_variable)
+            output = model.forward(img_variable)
+            loss_cal, losses_cal = cal_loss(output, tar_label_variable, targeted)
+            best_losses, best_adv_x = track_best(best_losses, best_adv_x, losses_cal, img_variable)
+            loss_cal.backward()
+            raw_grad = img_variable.grad.data
+            grad_norm = torch.max(
+                   raw_grad.view(raw_grad.size(0), -1).norm(2, 1), torch.tensor(1e-9).cuda())
+            grad_dir = raw_grad/grad_norm.view(raw_grad.size(0),1,1,1)
+            adv_temp = img_variable.data +  -1 * eps_step * grad_dir
+            # Clipping total perturbation
+            total_grad = adv_temp - image_tensor
+            total_grad_norm = torch.max(
+                   total_grad.view(total_grad.size(0), -1).norm(2, 1), torch.tensor(1e-9).cuda())
+            total_grad_dir = total_grad/total_grad_norm.view(total_grad.size(0),1,1,1)
+            total_grad_norm_rescale = torch.min(total_grad_norm, torch.tensor(eps_max).cuda())
+            clipped_grad = total_grad_norm_rescale.view(total_grad.size(0),1,1,1) * total_grad_dir
+            x_adv = image_tensor + clipped_grad
+            x_adv = torch.clamp(x_adv, clip_min, clip_max)
+            img_variable.data = x_adv
+
         best_losses, best_adv_x = track_best(best_losses, best_adv_x, losses_cal, img_variable)
-        loss_cal.backward()
-        raw_grad = img_variable.grad.data
-        grad_norm = torch.max(
-               raw_grad.view(raw_grad.size(0), -1).norm(2, 1), torch.tensor(1e-9).cuda())
-        grad_dir = raw_grad/grad_norm.view(raw_grad.size(0),1,1,1)
-        adv_temp = img_variable.data +  -1 * eps_step * grad_dir
-        # Clipping total perturbation
-        total_grad = adv_temp - image_tensor
-        total_grad_norm = torch.max(
-               total_grad.view(total_grad.size(0), -1).norm(2, 1), torch.tensor(1e-9).cuda())
-        total_grad_dir = total_grad/total_grad_norm.view(total_grad.size(0),1,1,1)
-        total_grad_norm_rescale = torch.min(total_grad_norm, torch.tensor(eps_max).cuda())
-        clipped_grad = total_grad_norm_rescale.view(total_grad.size(0),1,1,1) * total_grad_dir
-        x_adv = image_tensor + clipped_grad
-        x_adv = torch.clamp(x_adv, clip_min, clip_max)
-        img_variable.data = x_adv
 
-    best_losses, best_adv_x = track_best(best_losses, best_adv_x, losses_cal, img_variable)
+        diff_array = np.array(x_adv.cpu())-np.array(image_tensor.data.cpu())
+        diff_array = diff_array.reshape(len(diff_array),-1)
 
-    diff_array = np.array(x_adv.cpu())-np.array(image_tensor.data.cpu())
-    diff_array = diff_array.reshape(len(diff_array),-1)
-    # print("peturbation= {}".format(
-    #    np.max(np.linalg.norm(diff_array,axis=1))))
+        img_variable.data = image_tensor_orig
+        # print("peturbation= {}".format(
+        #    np.max(np.linalg.norm(diff_array,axis=1))))
     return best_adv_x
+
+
+def hybrid_attack(matched_x, ez, m, data_loader, eps):
+    # Fill up a tensor with actual matched data
+    matched_data = []
+    matched_labels = []
+    for idx in m[~ez]:
+        x_curr, y_curr, _, _, _ = data_loader[idx]
+        matched_data.append(x_curr)
+        matched_labels.append(y_curr)
+    matched_data = torch.stack(matched_data).cuda()
+    
+    # Construct the perturbation
+    diff_vec = matched_data-matched_x
+    diff_vec_norm = torch.max(diff_vec.view(diff_vec.size(0), -1).norm(2, 1), torch.tensor(1e-9).cuda())
+    diff_vec_dir = diff_vec/diff_vec_norm.view(diff_vec.size(0),1,1,1)
+    x_mod = matched_x + eps * diff_vec_dir
+
+    return x_mod
+
